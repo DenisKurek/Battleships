20dda8c007287e25f7b48ce04261dc60cc2f024a
Denis Kurek <denis4kurek@gmail.com>
2022-03-17 15:17:53 +0100

    dodanie klasy Game

--- src/main/java/org/example/backedn/Board.java
+++ src/main/java/org/example/backedn/Board.java
@@ -2,8 +2,11 @@ package org.example.backedn;
 
 public class Board {
     protected int BOARD_SIZE =10;
+    private final int NUMBER_OF_SHIPS = 5;
+    private final int MAX_SHIP_SIZE = 5;
     protected double CELL_SIZE = 40;
-    private Cell[][] board;
+    private Cell[][] cells;
+    private Ship[] ships;
     public Board(){
         initialize();
     }
@@ -13,10 +16,22 @@ public class Board {
         initialize();
     }
     private void initialize(){
-        this.board = new Cell[BOARD_SIZE][BOARD_SIZE];
+        initializeCells();
+        initializeShips();
+    }
+
+    private void initializeShips() {
+        this.ships = new Ship[NUMBER_OF_SHIPS];
+        for(int i=0;i<NUMBER_OF_SHIPS;i++){
+            this.ships[i] = new Ship(i,i,CELL_SIZE,CELL_SIZE*(Math.max(MAX_SHIP_SIZE-i,0)));
+        }
+    }
+
+    private void initializeCells() {
+        this.cells = new Cell[BOARD_SIZE][BOARD_SIZE];
         for (int i = 0;i <BOARD_SIZE;i++){
             for (int j = 0;j <BOARD_SIZE;j++){
-                this.board[i][j] = new Cell(i*CELL_SIZE, j*CELL_SIZE,CELL_SIZE);
+                this.cells[i][j] = new Cell(i*CELL_SIZE, j*CELL_SIZE,CELL_SIZE);
             }
         }
     }
